de9e80b3ae0e94e6fe8eba7ece28c7ecec6119eb
sdputurn <sdp.uturn@gmail.com>
2023-12-14 00:12:07 +0530

    running script from pipeline

--- test1.sh
+++ test1.sh
@@ -3,5 +3,5 @@ echo "new code fix in master"
 echo "welcome to github"
 echo "merge work due to changes at last line. letsee now"
 echo "code fix missing in this commit"
-echo "adding commit statuses2.1"
+echo "adding commit statuses2.1.1"
 
